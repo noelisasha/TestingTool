3603f0cdf18d896be82385bfbe7be849b95a4814
Hernan <v.andinohernan@gmail.com>
2020-10-11 17:25:01 -0300

    Cambio en el panel
    Se agregaron los combos de metodo y clase

--- src/frontend/ProbarPantalla.java
+++ src/frontend/ProbarPantalla.java
@@ -1,9 +1,26 @@
 package frontend;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
+import java.awt.Choice;
+import java.awt.Panel;
+import javax.swing.JCheckBoxMenuItem;
 
 public class ProbarPantalla {
 	
 	public static void main(String[] args) {
 		PantallaGrafica pantalla = new PantallaGrafica();
+		pantalla.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		Panel panel = new Panel();
+		pantalla.getContentPane().add(panel);
+		
+		Choice choice_1 = new Choice();
+		panel.add(choice_1);
+		
+		Choice choice = new Choice();
+		panel.add(choice);
 		pantalla.setVisible(true);
 	}
 

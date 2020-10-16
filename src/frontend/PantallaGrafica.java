8c08daff3de286f79db3b1a06e76a24cee7ac747
LuArtale <luartale@hotmail.com>
2020-10-15 23:51:14 -0300

    interfaz arreglada

--- src/frontend/PantallaGrafica.java
+++ src/frontend/PantallaGrafica.java
@@ -59,9 +59,9 @@ public class PantallaGrafica extends JFrame{
 		JPanel panel = new JPanel();
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{17, 180, 50, 180, 50, 0, 0};
-		gbl_panel.rowHeights = new int[]{38, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{38, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
 		JLabel lblNewLabel_2 = new JLabel("Resultados del Testeo");
--- src/frontend/ProbarPantalla.java
+++ src/frontend/ProbarPantalla.java
@@ -13,14 +13,6 @@ public class ProbarPantalla {
 		PantallaGrafica pantalla = new PantallaGrafica();
 		pantalla.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		Panel panel = new Panel();
-		pantalla.getContentPane().add(panel);
-		
-		Choice choice_1 = new Choice();
-		panel.add(choice_1);
-		
-		Choice choice = new Choice();
-		panel.add(choice);
 		pantalla.setVisible(true);
 	}
 
